325060bf977b92e70288930dafaec8db398c7251
Charles Simanjuntak <decharvi.charles@gmail.com>
2021-06-09 11:13:00 +0700

    Create 4-en.js

---
+++ pages/internet-bisinis/4-en.js
@@ -0,0 +1,29 @@
+
+<script type="text/javascript">
+function scroll_to_div(div_id)
+{
+ $('html,body').animate(
+ {
+  scrollTop: $("#"+div_id).offset().top
+ },
+ 'slow');
+}
+</script>
+
+
+<script>
+$(document).ready(function() {
+$(".downbutton").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".up").offset().top
+     }, 1500);
+ });
+});
+$(document).ready(function() {
+$(".up").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".downbutton").offset().top
+     }, 1000);
+ });
+});
+</script>
