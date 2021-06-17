b2c583e652682ad205efd4b44751baa99d9cdccb
Charles Simanjuntak <decharvi.charles@gmail.com>
2021-06-17 16:45:42 +0700

    Update page - beranda.php

--- page-templates/page - beranda.php
+++ page-templates/page - beranda.php
@@ -37,19 +37,23 @@ Template Name: Beranda
                 <div class="main__header">
                     
                      <?php if(ICL_LANGUAGE_CODE == 'id'){ ?>
-                          <h2><?= __("Hubungi Kami",'myrepublic_theme') ?> <strong>1500-818</strong></h2>
+                          <!-- <h2><?= __("Hubungi Kami",'myrepublic_theme') ?> <strong>1500-818</strong></h2> -->
+                          <h2><?= __("Hubungi Kami",'myrepublic_theme') ?> <strong><a href="https://api.whatsapp.com/send/?phone=6288981500818&text=Halo+MyRepublic" target="_blank" style="color:#fff;">08898-1500818</a></strong></h2>
                      <?php }elseif(ICL_LANGUAGE_CODE == 'en'){ ?>
-                         <h2><?= __("Give us a call",'myrepublic_theme') ?> <strong>1500-818</strong></h2>
+                         <!-- <h2><?= __("Give us a call",'myrepublic_theme') ?> <strong>1500-818</strong></h2> -->
+                         <h2><?= __("Give us a call",'myrepublic_theme') ?> <strong><a href="https://api.whatsapp.com/send/?phone=6288981500818&text=Halo+MyRepublic" target="_blank">08898-1500818</a></strong></h2>
                      <?php } ?>
                      
            
                 </div>
                 <div class="main__btn">
-                    <a href="tel:0211500818">
-                        <i class="fas fa-phone"></i>
-                        <?= __("1500-818",'myrepublic_theme') ?>
+                    <!-- <a href="tel:0211500818"> -->
+                    <a href="https://api.whatsapp.com/send/?phone=6288981500818&text=Halo+MyRepublic" target="_blank">
+                        <!-- <i class="fas fa-phone"></i> -->
+                        <i class="fab fa-whatsapp" aria-hidden="true"></i>
+                        <?= __("08898 1500818",'myrepublic_theme') ?>
                     </a>
-                    <a href="javascript:void(0)" data-toggle='modal' data-target='#scheduleCall'>
+                    <a class="hidden" href="javascript:void(0)" data-toggle='modal' data-target='#scheduleCall'>
                         <i class="fas fa-clock"></i>
                           <?php if(ICL_LANGUAGE_CODE == 'id'){ ?>
                          <?= __("Jadwalkan panggilan",'myrepublic_theme') ?>
