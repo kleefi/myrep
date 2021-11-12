d139c9451de91f62bf123dfe1f818d579449dab3
Charles Simanjuntak <decharvi.charles@gmail.com>
2021-11-12 14:38:13 +0700

    Create index.php

---
+++ ncop-api/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kirim</title>
+</head>
+<body>
+  <form action="kirim.php" method="POST">
+    <input type="text" name="nama_pegawai" placeholder="Nama"><br>
+    <input type="text" name="umur_pegawai" placeholder="Umur"><br>
+    <input type="text" name="jabatan" placeholder="Jabatan"><br>
+    <input type="submit" value="Kirim">
+</form>
+</body>
+</html>
